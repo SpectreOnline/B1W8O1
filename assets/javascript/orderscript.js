229db4b816add29e96ff447638394c386c004c7b
Tido <99051567@mydavinci.nl>
2019-10-30 15:32:46 +0100

    Third commit
    fixed a bug where the total would display NaN if not all drinks were selected.

--- assets/javascript/orderscript.js
+++ assets/javascript/orderscript.js
@@ -1,6 +1,9 @@
 var AantalFris = 0;
 var AantalBier = 0;
 var AantalWijn = 0;
+var TotaalFris = 0;
+var TotaalBier = 0;
+var TotaalWijn = 0;
 var TotaalBedrag = 0;
 
 //hier beginnen de functies
@@ -8,7 +11,6 @@ function bestellingPhase1() {
     var BestellingDrinken = prompt("Wat zou u willen bestellen? \n Uw opties zijn: \n Fris, bier en wijn \n als u klaar bent met bestellen typ dan stop in").toLowerCase();
     if (BestellingDrinken == "fris") {
         AantalFris += parseInt(prompt("Hoeveel dranken fris wilt u bestellen?"));
-        console.log(AantalFris);
         bestellingPhase1();
     } else if (BestellingDrinken == "bier") {
         AantalBier += parseInt(prompt("Hoevel glazen bier wilt u bestellen?"));
@@ -26,23 +28,22 @@ function bestellingPhase1() {
 
 function bestellingPhase2() {
     if (AantalFris != 0) {
-        var TotaalFris = AantalFris * 1.50
+        TotaalFris = parseInt(AantalFris * 1.50)
         document.write("Uw totaal voor Fris is: ", TotaalFris, "<br>");
     }
 
     if (AantalBier != 0) {
-        var TotaalBier = AantalBier * 2
+        TotaalBier = parseInt(AantalBier * 2)
         document.write("Uw totaal voor Bier is: ", TotaalBier, "<br>");
     }
 
     if (AantalWijn != 0) {
-        var TotaalWijn = AantalWijn * 2.75
+        TotaalWijn = parseInt(AantalWijn * 2.75)
         document.write("Uw totaal voor Wijn is: ", TotaalWijn, "<br>");
     }
-    console.log(AantalBier);
-    console.log(AantalWijn);
+
     if (AantalFris != 0 || AantalWijn != 0 || AantalBier != 0) {
-        parseInt(TotaalBedrag = TotaalFris + TotaalBier + TotaalWijn);
+        parseInt(TotaalBedrag += TotaalFris += TotaalBier += TotaalWijn);
         document.write("Uw totaal bedrag is: " + TotaalBedrag);
     }
 }
